5740d1b108e8f4cebb5b3a678b5ec9ab1e983302
watanabe-sota <819sota@gmail.com>
2021-09-05 09:19:28 +0900

    add:driver

---
+++ src/driver/blogDriver.ts
@@ -0,0 +1,10 @@
+import BlogDriver, { BlogJson } from "@interface/driver/blogDriver"
+import { RestClientInterface } from "@lib/restClient/restClient"
+
+export default class BlogDriverImpl implements BlogDriver {
+  constructor(private readonly restClient: RestClientInterface) {}
+  async findAll(): Promise<BlogJson> {
+    const res = await this.restClient.get<BlogJson>("/api/v1/blog")
+    return res
+  }
+}
