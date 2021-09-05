41aabbfeb1b77f8d158a50cc9251646015d63152
watanabe-sota <819sota@gmail.com>
2021-09-05 10:51:20 +0900

    add:test page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useFetchBlog } from "@presentation/hooks/useFetchBlog"
+import { Article } from "@domain/article"
 
 const Index: React.FC = () => {
+  const { fetchBlog } = useFetchBlog()
+  const [articles, setArticles] = useState<Article[]>()
+
+  useEffect(() => {
+    fetchBlog().then((r) => setArticles(r.getContents))
+  })
+
   return (
-    <div className={"p-16 bg-indigo-400 h-screen"}>
-      <div className={"text-center font-bold text-9xl"}>テスト</div>
+    <div className={"p-16 h-screen"}>
+      <div className={"text-center font-black font-bold"}>{JSON.stringify(articles)}</div>
     </div>
   )
 }
