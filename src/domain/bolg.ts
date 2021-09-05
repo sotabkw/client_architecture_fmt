69b9bc77ee587c5ed0933929083f8378fa0e532c
watanabe-sota <819sota@gmail.com>
2021-09-05 10:51:35 +0900

    fix:blog entity

--- src/domain/bolg.ts
+++ src/domain/bolg.ts
@@ -3,6 +3,10 @@ import { Article } from "@domain/article"
 export class Blog {
   constructor(private readonly contents: Article[], private readonly totalCount: number) {}
 
+  get getContents() {
+    return this.contents
+  }
+
   get getArticleCount() {
     return this.totalCount
   }
